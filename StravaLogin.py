bf91456bdd757caf227d0d6fb6ab167d90dfbc9e
Jurenkova <simona.jurenkova@accenture.com>
2019-06-23 16:16:39 +0200

    add_selenium_script

---
+++ StravaLogin.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.support.ui import Select
+# from selenium.webdriver import ActionChains
+# from selenium.webdriver import TouchActions
+# from selenium.common.exceptions import ElementClickInterceptedException
+
+driver = webdriver.Chrome()
+
+# open strava.com
+driver.get("http://www.strava.com")
+assert "Strava | Run and Cycling Tracking on the Social Network for Athletes" in driver.title
+assert "No results found." not in driver.title
+
+# driver.implicitly_wait(60)
+driver.maximize_window()
+wait = WebDriverWait(driver, 10)
+element = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, 'Sign up with Facebook')))
+
+driver.save_screenshot('screenshot.png')
+
+
+# open login screen
+fb_signup = driver.find_element_by_link_text('Sign up with Facebook')
+fb_signup.click()
+
+wait = WebDriverWait(driver, 10)
+element = wait.until(EC.element_to_be_clickable((By.ID, 'email')))
+
+driver.save_screenshot('screenshot2.png')
+
+# enter login details
+username = driver.find_element_by_id('email')
+username.send_keys("simona.jurenkova@gmail.com")
+password = driver.find_element_by_id('pass')
+password.send_keys("S1mcaL1mca!1")
+
+login = driver.find_element_by_id('loginbutton')
+login.click()
+
+
+# open my Strava profile
+wait = WebDriverWait(driver, 10)
+# element = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'athlete-name')))
+
+# by_class = driver.find_element_by_class_name('athlete-name')
+# by_class = driver.find_element_by_class_name('avatar-img')
+# by_class.click()
